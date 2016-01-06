4cb524d9f4b3dcaa06c43142f24a68ea7aee2fee
Noah <nba4191@gmail.com>
2016-01-06 19:35:58 +0000

    Add movie validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+    validates_presence_of :title
+    validates_presence_of :format
+    validates :length, presence: true, numericality: true
+    validates :release_year, presence: true, numericality: true
+    validates :rating, presence: true, numericality: true
 end
