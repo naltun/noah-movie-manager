e18be0cb133a715571f841baa8ddfa966acf3c09
Noah <nba4191@gmail.com>
2016-01-07 03:40:34 +0000

    Update movie model validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,6 @@ class Movie < ActiveRecord::Base
     validates_presence_of :title, length: { minimum: 1, maximum: 50 }
     validates_presence_of :format, length: { minimum: 1, maximum: 499 }
     validates :length, presence: true, numericality: true
-    validates :release_year, presence: true, numericality: true, length: { minimum: 1800, maximum: 2100 }
+    validates :release_year, presence: true, numericality: true, inclusion: { in: 1800..2100 }
     validates :rating, presence: true, numericality: true
 end
